a1cffd3d937c421ddf2b9d68907f89c23558b0b2
= <tsmith@argo.ai>
2021-06-04 12:26:01 -0700

    Trying to get tidal locking building on another computer

--- run_build.sh
+++ run_build.sh
@@ -1,12 +1,14 @@
 sudo rm -rf build
 mkdir build && cd build
 cmake .. \
-    -DIMGUI_DIR=/home/tasman/Coding/ImGui/imgui \
     -DCMAKE_INSTALL_PREFIX=/usr \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 make
 sudo make install # sudo may be needed
 cd ..
+
+
+    # -DIMGUI_DIR=/home/tasman/Coding/ImGui/imgui \
 
     # -BOX2D_LIBRARY=/home/tasman/Coding/Box2d/box2d \
     # -BOX2D_INCLUDE_DIR=/home/tasman/Coding/Box2d/box2d
